80c9a02b62fbd9f2a08bd3d1a0e123601f223efc
Dave Dribin <dave@dribin.org>
2008-06-28 18:42:59 -0500

    Add DDFoundation.h

---
+++ lib/DDFoundation.h
@@ -0,0 +1,17 @@
+/*
+ *  DDFoundation.h
+ *  DDFoundation
+ *
+ *  Created by Dave Dribin on 6/28/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "DDInvocationGrabber.h"
+#import "DDObserverDispatcher.h"
+#import "DDSequenceComparator.h"
+#import "DDTemporaryDirectory.h"
+#import "NSArray+DDExtensions.h"
+#import "NSData+DDExtensions.h"
+#import "NSObject+DDExtensions.h"
+#import "NSString+DDExtensions.h"
